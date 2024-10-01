0626a903d64b26460a49f13b6e1f03a939f4421d
KevFitto <kyfittoria@uamv.edu.ni>
2024-10-01 17:09:40 -0600

    nose

---
+++ src/main/java/model/Test.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Test {
+    private static Test instance;
+
+    private Test() {
+    }
+
+    public static Test getInstance() {
+        if (instance == null) {
+            instance = new Test();
+        }
+        return instance;
+    }
+
+
+}
